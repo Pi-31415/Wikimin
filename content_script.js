57ceb8d7a783ff4796dd344ee5fc8f44a023792d
Pi <paingthetko.pi@gmail.com>
2021-01-17 20:24:41 +0630

    final commit

--- content_script.js
+++ content_script.js
@@ -2,26 +2,20 @@ var url = window.location.href;
 
 
 
-if (url.includes("wikipedia.org") || url.includes("wikiwand.com")) {
+if (url.includes("wikiwand.com")) {
 
 
-	
+	//var a = document.getElementsByTagName('a');
 
-	var a;
-	//switch
-	if(url.includes("wikipedia.org")){
-		a = document.getElementsByTagName('a');
-	}
-	else{
-		a = document.getElementsByClassName("int-link");
-	}
-
+	var a = document.getElementsByClassName("int-link");
 	
 	for (var idx = 0; idx < a.length; ++idx) {
 		//console.log(a[idx].href);
-		a[idx].setAttribute("style", "color:#313131;");
+		a[idx].setAttribute("style", "color:#313131");
 	}
 	
+	a = document.getElementsByTagName('nav')[0].remove();
+
 
 	a = document.getElementsByTagName('p');
 	
